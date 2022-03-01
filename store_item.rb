d62f008495184feee3dccc6cc5535e7ed46f7b91
nospame <mapson.evan@gmail.com>
2022-03-01 11:48:39 -0800

    clean up line spacing

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,5 @@
 item_1 = {type: "pencil", brand: "Ticonderoga", color: "yellow", price: 3}
 item_2 = {type: "pen", brand: "Pilot", color: "purple", price: 5}
 item_3 = {:type => "pen", :brand => "Bic", color: "black", :price => 2}
+
 p "The #{item_1[:brand]} #{item_1[:type]} costs $#{item_1[:price]} and comes in #{item_1[:color]}."
