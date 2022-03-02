f10740da173c86fed3f21fadbc95158539ddc9df
nospame <mapson.evan@gmail.com>
2022-03-02 10:20:30 -0800

    change to attr_reader/attr_writer methods

--- store_item.rb
+++ store_item.rb
@@ -5,6 +5,9 @@
 # p "The #{item_1[:brand]} #{item_1[:type]} costs $#{item_1[:price]} and comes in #{item_1[:color]}."
 
 class Item
+  attr_reader :type, :brand, :color, :price
+  attr_writer :price, :color
+
   def initialize(input_type, input_brand, input_color, input_price)
     @type = input_type
     @brand = input_brand
@@ -12,38 +15,18 @@ class Item
     @price = input_price
   end
 
-  def type
-    @type
-  end
-
-  def brand
-    @brand
-  end
-
-  def color
-    @color
-  end
-
-  def price
-    @price
-  end
-
-  def price=(input_price)
-    @price = input_price
-  end
-
-  def color=(input_color)
-    @color = input_color
-  end
-
 end
 
 item1 = Item.new("pencil", "Ticonderoga", "yellow", 3)
 
-# p item1.price
+# puts "old price:"
+# puts item1.price
 # item1.price = 4.50
-# p item1.price
-
-p item1.color
-item1.color = "purple"
-p item1.color
+# puts "new price:"
+# puts item1.price
+# puts "-----"
+# puts "old color:"
+# puts item1.color
+# item1.color = "purple"
+# puts "new color:"
+# puts item1.color
