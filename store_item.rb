e5431ce073aaa931b0595de907e5e5a2d0477fdd
nospame <mapson.evan@gmail.com>
2022-03-02 10:40:14 -0800

    add input_options hash

--- store_item.rb
+++ store_item.rb
@@ -8,16 +8,22 @@ class Item
   attr_reader :type, :brand, :color, :price
   attr_writer :price, :color
 
-  def initialize(input_type, input_brand, input_color, input_price)
-    @type = input_type
-    @brand = input_brand
-    @color = input_color
-    @price = input_price
+  def initialize(input_options)
+    @type = input_options[:type]
+    @brand = input_options[:brand]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
 
 end
 
-item1 = Item.new("pencil", "Ticonderoga", "yellow", 3)
+# item1 = Item.new("pencil", "Ticonderoga", "yellow", 3)
+item1 = Item.new(
+  type: "pencil", 
+  brand: "Ticonderoga", 
+  color: "yellow", 
+  price: 3
+)
 
 # puts "old price:"
 # puts item1.price
