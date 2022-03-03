eb99466dc06d834a2294b1ab120f895452be5d63
nospame <mapson.evan@gmail.com>
2022-03-03 13:19:10 -0800

    add share your score option

--- wordle.rb
+++ wordle.rb
@@ -1,5 +1,7 @@
-answer = "GLASS"
+answer = "PIANO"
 guess = ""
+turn_count = 0
+shareable = []
 
 6.times do
   puts "Please enter a guess:"
@@ -16,7 +18,11 @@ guess = ""
     end
     index += 1
   end 
+  
+  turn_count += 1
+  shareable << squares
   puts squares
+
   if guess == answer
     puts "You win!"
     break
@@ -26,4 +32,16 @@ end
 if guess != answer
   puts "You lose."
   puts "The word was #{answer}"
+end
+
+puts "Share your score? (Y/N)"
+input = gets.chomp
+puts ""
+if input.upcase == "Y"
+  if guess == answer
+    puts "Wordle #{turn_count} / 6"
+  else
+    puts "Wordle #{turn_count} / *"
+  end
+  puts shareable
 end
