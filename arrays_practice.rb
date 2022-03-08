0cf5805933fa3cb3c6d4e24cd50cafd35dec0aed
nospame <mapson.evan@gmail.com>
2022-03-07 16:31:34 -0800

    complete arrays practice

--- arrays_practice.rb
+++ arrays_practice.rb
@@ -17,17 +17,43 @@
 # end
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
-
+# array = [1]
+# array += [3, 4]
+# p array
 
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+# array = ["alpha", "beta", "gamma"]
+# array[2] = array[2].upcase
+# p array
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+# array = ["Jeff", "Beck", "Albert"]
+# i = 0
+# while i < array.length
+#   p array[i]
+#   i += 1
+# end
 
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+# array = ["Kevin", "Cleo"]
+# array << "Clifford"
+# p array
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+# numbers = [1, 9, 8, 12, 263]
+# numbers[0] *= 10
+# p numbers
 
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+# array = [4, 27]
+# i = 0
+# while i < array.length
+#   p array[i]
+#   i += 1
+# end
 
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+# countries = ["Chile", "Ecuador", "Germany"]
+# countries << "Brazil"
+# p countries
 
